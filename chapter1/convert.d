aa726558f491f980d0588cf12980fc1c4cc1e7e5
bcumming <bcumming@cscs.ch>
2013-11-14 12:08:27 +0100

    new conversion example

---
+++ chapter1/convert.d
@@ -0,0 +1,17 @@
+#!/usr/bin/rdmd
+
+import std.stdio;
+
+void main(){
+    immutable inchesPerFoot = 12;
+    immutable cmPerInch = 2.54;
+    // this also works:
+    //immutable double cmPerInch = 2.54;
+
+    foreach(feet; 5 .. 7){
+        foreach(inches; 0 .. inchesPerFoot){
+            writeln(feet, "'", inches, "''\t", (feet*inchesPerFoot + inches)*cmPerInch);
+        }
+    }
+}
+
